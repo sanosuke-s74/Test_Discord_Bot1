b914a4b7314fa50110675b6773ea3bd548c038cf
unknown <abel.tovar2080@gmail.com>
2019-01-22 20:59:56 -0600

    expanding dice roll (incomplete)

--- bot.js
+++ bot.js
@@ -12,12 +12,12 @@ var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
-bot.on('ready', function (evt) {
+bot.on('ready', async function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', async function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
@@ -25,12 +25,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0];
 
         args = args.splice(1);
+
         switch (cmd.toLowerCase()) {
             // !ping
             case 'ping':
                 // console.log(bot);
                 var avatarID = bot.users[userID].avatar;
-                console.log(bot.users[userID].avatar);
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!' + ' https://cdn.discordapp.com/avatars/' + userID + '/a_' + avatarID + '.png'
@@ -38,18 +38,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
             // Just add any case commands if you want to..
             case 'diceroll':
-                var dicenum = Math.floor(Math.random() * 7) + 1;
-                if (dicenum == 7) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: "Boogie's a bitch!"
-                    });
-                } else {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: user + ' rolled a ' + dicenum
-                    });
+                let msgOutput = "";
+                let diceType = args[0].substr(1);
+                let numDiceThrows = parseInt(args[1], 10);
+                if (numDiceThrows <= 0 || numDiceThrows > 10) {
+                    numDiceThrows = 5;
+                    msgOutput = msgOutput + numDiceThrows.toString() + ""
                 }
+
+                msgOutput = user.toString() + ' is throwing a d' + diceType.toString() + ' ' + numDiceThrows.toString() + ' time(s)\n';
+
+                for (let x = 0; x < numDiceThrows; x++) {
+                    let dicenum = Math.floor(Math.random() * diceType) + 1;
+                    msgOutput = msgOutput + "Throw #" + (x + 1).toString() + ": " + dicenum.toString() + "\n";
+                }
+
+                bot.sendMessage({
+                    to: channelID,
+                    message: msgOutput
+                });
+
+                console.log(msgOutput);
                 break;
         }
     }
